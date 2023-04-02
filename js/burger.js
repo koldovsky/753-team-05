bc01cc1223c3808b54b282d2f466a45d8c43a1f0
IhorMalkov <122704869+IhorMalkov@users.noreply.github.com>
2023-04-02 21:56:02 +0000

    changed order

--- js/burger.js
+++ js/burger.js
@@ -2,14 +2,14 @@ const burgerMenu = document.querySelector(".header__menu");
 const burgerIcon = document.querySelector(".header__burger");
 const menuLinks = document.querySelectorAll(".menu__link");
 
+burgerIcon.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  burgerIcon.classList.toggle("active");
+});
+
 menuLinks.forEach(link => {
   link.addEventListener("click", () => {
     burgerMenu.classList.remove("active");
     burgerIcon.classList.remove("active");
   });
-});
-
-burgerIcon.addEventListener("click", () => {
-  burgerMenu.classList.toggle("active");
-  burgerIcon.classList.toggle("active");
 });
