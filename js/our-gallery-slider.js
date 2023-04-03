520bea47335cbac643b9ef2665829027dd9e3cc0
ohhyana <126813314+ohhyana@users.noreply.github.com>
2023-04-03 04:37:45 +0000

    add our-gallery-slider.js

---
+++ js/our-gallery-slider.js
@@ -0,0 +1,63 @@
+(function () {
+    const slides = [
+        '<div class="slide"><img src="img/neon-woman.png" alt="Neon Woman"></div>',
+        ' <div class="slide"><img src="img/club-party.png" alt="Club Party"></div>',
+        ' <div class="slide"><img src="img/dj-controller.png" alt="DJ Controller"></div>',
+        ' <div class="slide"><img src="img/club-concert.png" alt="Club Concert"></div>',
+      ];
+      
+      const carousel = document.querySelector('.our-gallery__slider');
+      const slideContainer = carousel.querySelector('.our-gallery__slider .our-gallery__slides');
+      
+      let currentSlide = 0;
+      
+      function renderSlides(slides) {
+        slideContainer.innerHTML = '';
+      
+        if (window.innerWidth > 768) {
+          slideContainer.innerHTML = slides[currentSlide] + slides[(currentSlide + 1) % slides.length];
+        } else {
+          slideContainer.innerHTML = slides[currentSlide];
+        }
+      
+        // update active class for dots
+        carousel.querySelectorAll('.our-gallery__dots span').forEach((dot, index) => {
+          if (index === currentSlide) {
+            dot.classList.add('active');
+          } else {
+            dot.classList.remove('active');
+          }
+        });
+      }
+      
+      function nextSlide() {
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+          currentSlide = 0;
+        }
+        renderSlides(slides);
+      }
+      
+      function prevSlide() {
+        currentSlide--;
+        if (currentSlide < 0) {
+          currentSlide = slides.length - 1;
+        }
+        renderSlides(slides);
+      }
+      
+      function updateSlide(index) {
+        currentSlide = index;
+        renderSlides(slides);
+      }
+
+      carousel.querySelectorAll('.our-gallery__dots span').forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+          updateSlide(index);
+        });
+      });
+      
+      window.addEventListener('resize', () => {
+        renderSlides(slides);
+      });
+}) ();
