dfecdddf15627af03cc08ae80baaf39f7b96b93b
IhorMalkov <122704869+IhorMalkov@users.noreply.github.com>
2023-04-03 23:54:37 +0000

    burger fixes

--- js/burger.js
+++ js/burger.js
@@ -2,14 +2,22 @@ const burgerMenu = document.querySelector(".header__menu");
 const burgerIcon = document.querySelector(".header__burger");
 const menuLinks = document.querySelectorAll(".menu__link");
 
-burgerIcon.addEventListener("click", () => {
+burgerIcon.addEventListener("click", function() {
   burgerMenu.classList.toggle("active");
   burgerIcon.classList.toggle("active");
+
+  if (burgerMenu.classList.contains("active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "initial";
+  }
 });
 
-menuLinks.forEach(link => {
-  link.addEventListener("click", () => {
+menuLinks.forEach(function(link) {
+  link.addEventListener("click", function() {
     burgerMenu.classList.remove("active");
     burgerIcon.classList.remove("active");
+    document.body.style.overflow = "initial";
   });
 });
+
