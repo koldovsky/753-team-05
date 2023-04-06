9789f2ab4fa9c9c7a71688f12a6e2372e66d4445
Cherevatiy Andriy <126006834+CherevatiyAndriy@users.noreply.github.com>
2023-04-06 09:12:22 +0000

    change js button-show-more

--- js/button-show-more.js
+++ js/button-show-more.js
@@ -1,11 +1,3 @@
-const button = document.querySelector('.welcome__button');
-const informationText = document.querySelector('.welcome__information-text');
-
-button.addEventListener('click', function() {
-  informationText.classList.toggle('show-more');
-  if (button.value === 'Show more') {
-    button.value = 'Show less';
-  } else {
-    button.value = 'Show more';
-  }
-});
+function goToSection() {
+  window.location.href = '#tickets'; 
+}
